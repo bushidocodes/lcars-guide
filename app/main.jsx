ac47393129841611c8e31411f6a09e1ccf69ef43
Sean McBride <spmcbride1201@gmail.com>
2017-02-28 22:33:44 -0500

    chore: Setup production build pipeline.

--- app/main.jsx
+++ app/main.jsx
@@ -6,14 +6,29 @@ import AppContainer from './components/AppContainer';
 import Episodes from './components/Episodes';
 import store from './store';
 
+// Require LCARS CSS as CommonJS module
+require('./style/lcars.css');
+
+// Routes implemented as PlainRoutes due to react-router bug with HMR
+// See https://github.com/reactjs/react-router-redux/issues/179
+const routes = {
+  path: '/',
+  component: AppContainer,
+  childRoutes: [
+    { path: '/seasons/:seasonId', component: Episodes }
+  ],
+  indexRoute: { onEnter: (nextState, replace) => replace('seasons/4') }
+};
+
 render(
   <Provider store={store}>
-    <Router history={hashHistory}>
-      <Route path="/" component={AppContainer} >
-        <Route path="seasons/:seasonId" component={Episodes} />
-        <IndexRedirect to="seasons/4" />
-      </Route>
-    </Router>
+    <Router history={hashHistory} routes={routes} />
   </Provider>,
   document.getElementById('main')
 );
+
+// Use WebPack HotModuleReloading if available
+// Commented out pending bug between React-Router and HMR
+// if (module.hot) {
+  // module.hot.accept();
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,59 @@
-const LiveReloadPlugin = require('webpack-livereload-plugin');
+const path = require('path');
+const webpack = require('webpack');
 
 // Engage Ward Speed to accelerate rebuilds, but at the cost of sourcemaps
 // If you need to engage in combat with bugs, drop out of Warp
 const WARP_SPEED_ENGAGED = false;
 
+// Read environment variables to know if we are in dev, test, or prod
+const DEVELOPMENT = process.env.NODE_ENV === 'development';
+const PRODUCTION = process.env.NODE_ENV === 'production';
+
+// Generate the entry point based on the environment variable
+const entry = PRODUCTION
+  ? ['./app/main.jsx']
+  : [
+    './app/main.jsx',
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:8080'
+  ];
+
+// Uglify and tree shake the bundle if in Production, allow Hot Module Reloading otherwise
+const plugins = PRODUCTION
+  ? [new webpack.optimize.UglifyJsPlugin()]
+  : [new webpack.HotModuleReplacementPlugin()];
+
+// Define a custom WebPack plugin to make the DEVELOPMENT and PRODUCTION values accessible as global variables
+//   in application code
+plugins.push(
+  new webpack.DefinePlugin({
+    DEVELOPMENT: JSON.stringify(DEVELOPMENT),
+    PRODUCTION: JSON.stringify(PRODUCTION)
+  })
+);
+
 module.exports = {
-  entry: './app/main.jsx',
-  output: {
-    path: __dirname,
-    filename: './public/bundle.js',
-  },
+  entry,
   context: __dirname,
   devtool: WARP_SPEED_ENGAGED ? 'cheap-module-eval-source-map' : 'source-map',
   resolve: {
     extensions: ['.js', '.jsx', '.json', '*']
   },
   module: {
-    rules: [{
-      test: /jsx?$/,
-      exclude: /node_modules/,
-      use: [{
-        loader: 'babel-loader',
-        options: {
-          presets: ['react', 'es2015', 'stage-2'],
-        },
-      }],
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['babel-loader'],
+      exclude: /node_modules/
+    }, {
+      test: /\.css$/,
+      loaders: ['style-loader', 'css-loader'],
+      exclude: /node_modules/
     }],
   },
-  plugins: [
-    new LiveReloadPlugin({ appendScriptTag: true }),
-  ],
+  plugins,
+  output: {
+    path: path.join(__dirname, 'public'),
+    publicPath: PRODUCTION ? '/' : '/public',
+    filename: 'bundle.js',
+  }
 };
