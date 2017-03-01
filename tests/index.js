e3c0d58d44a5f5b277d3ee7975add79188acb6e3
Sean McBride <spmcbride1201@gmail.com>
2017-03-01 12:28:09 -0500

    tests: Implement a few tests.

--- tests/index.js
+++ tests/index.js
@@ -6,13 +6,16 @@ import sinon from 'sinon';
 import { mount, shallow, render } from 'enzyme';
 import { Table, Thead, Th } from 'reactable';
 import store from '../app/store';
-import { fetchImdbData } from '../app/actions/imdb.js';
 import Episodes from '../app/components/Episodes.jsx';
+import { generateURL } from '../app/utils';
+
+const should = require('chai').should();
+var xhr;
+var requests;
 
 chai.use(chaiEnzyme());
 
 describe('<Episodes />', () => {
-  xit('renders a spinner when Redux store is in loading state', () => { });
   xit('renders an error message when Redux store is in error state', () => { });
   // Something is wrong with my syntax in this test.
   // Currently returns false negative.
@@ -34,11 +37,10 @@ describe('<Episodes />', () => {
     };
     const wrapper = render(<Episodes store={store} routeParams={routeParams} imdb={dummyImdb} />);
     const seasonHeader = wrapper.find('h3')[0].children[0].data;
-    expect(seasonHeader).to.be.a('string');
-    expect(seasonHeader).to.contain('Season 4');
+    seasonHeader.should.be.a('string');
+    seasonHeader.should.contain('Season 4');
   });
 
-  // This test is pretty much useless right now because Table is rendered regardless of props
   it('renders a Reactable Table when Redux store received IMDB results', () => {
     const routeParams = { seasonId: '4' };
     const dummyImdb = {
@@ -59,15 +61,24 @@ describe('<Episodes />', () => {
   });
 });
 
-describe('fetchImdbData Thunk', () => {
-  xit('creates a properly formed URL based on a valid Star Trek TNG season', () => { });
-  it('uses fetch to dispatch an HTTP get request to the IMDB server', () => {
-    // window.fetch = (url) => Promise.resolve();
-    const fetchStub = sinon.stub(window, 'fetch');
-    store.dispatch(fetchImdbData(1));
-    fetchStub.should.have.been.called();
-    // fetchStub.should.have.been.calledWith('http://www.omdbapi.com/?i=tt0092455&season=1&ref_=tt_eps_sn_1');
+describe('generateURL', () => {
+  it('creates a properly formed URL based on a valid Star Trek TNG season', () => {
+    generateURL('1').should.equal('http://www.omdbapi.com/?i=tt0092455&season=1&ref_=tt_eps_sn_1');
   });
+});
+
+describe('fetchImdbData Thunk', () => {
+  // Mock XHR using Sinon
+  beforeEach(() => {
+    xhr = sinon.useFakeXMLHttpRequest();
+    requests = [];
+    xhr.onCreate = req => requests.push(req);
+  });
+  // Clean up the globals
+  afterEach(() => {
+    xhr.restore();
+  });
+  xit('uses fetch to dispatch an HTTP get request to the IMDB server', () => {});
   xit('dispatches imdbIsLoading to set store\'s loading state to true upon HTTP GET', () => { });
   xit('dispatches imdbIsLoading to set store\'s loading state to false upon successful response', () => { });
   xit('dispatches imdbIsLoading to set store\'s loading state to false and dispatches imdbHasErrored to set store\'s error state upon failed response', () => { });
